210bad7b0ac0727e2566dc972a08873f40069c5d
AndryDevelopeer <malogorya@mail.ru>
2021-03-09 22:52:02 +0500

    s

--- src/Section/Section.js
+++ src/Section/Section.js
@@ -8,6 +8,9 @@ import work5 from "../img/9.jpg"
 import work2 from "../img/5.jpg"
 import work3 from "../img/6.jpg"
 import work4 from "../img/7.jpg"
+import work6 from "../img/8.jpg"
+import work7 from "../img/10.jpg"
+import work8 from "../img/11.jpg"
 
 
 function Section() {
@@ -32,7 +35,7 @@ function Section() {
                     </div>
                 </div>
             </div>
-            <div>
+            <div className={s.myWork}>
                 <div>
                     <h2>Work</h2>
                 </div>
@@ -48,7 +51,6 @@ function Section() {
                 <div className={s.work_hidden}>
                     <img className={s.img_work} src={work} alt="work"/>
 
-
                 </div>
                 <div className={s.desctop}>
                     <div className={s.work}>
@@ -63,6 +65,15 @@ function Section() {
                     </div>
 
                 </div>
+                <div className={s.desctop}>
+                    <div className={s.work}>
+                        <img className={s.img_work} src={work6} alt="work6"/>
+                        <img className={s.img_work} src={work7} alt="work7"/>
+                        <img className={s.img_work} src={work8} alt="work8"/>
+
+                    </div>
+
+                </div>
 
 
             </div>
@@ -71,7 +82,9 @@ function Section() {
                 <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab adipisci aperiam assumenda, autem
                     blanditiis eaque et fugit laborum maiores necessitatibus nesciunt nostrum pariatur provident quaerat
                     quia tempora tempore tenetur voluptate.
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci delectus eaque expedita itaque nemo nesciunt quisquam rerum. Aliquam dolore doloremque, error eum maiores recusandae tenetur totam. Adipisci assumenda dolores quisquam.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci delectus eaque expedita itaque
+                    nemo nesciunt quisquam rerum. Aliquam dolore doloremque, error eum maiores recusandae tenetur totam.
+                    Adipisci assumenda dolores quisquam.
                 </div>
             </div>
         </div>
